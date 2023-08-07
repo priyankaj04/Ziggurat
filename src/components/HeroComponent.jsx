358d199f2ba78a9004d6120326b2bb1964ee3f60
priyankaj04 <priyankaj2002o4@gmail.com>
2023-08-07 08:59:33 +0530

    components added

---
+++ src/components/HeroComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import heroimg from '../imgs/ziggurat-03-m.webp'
+
+function HeroComponent() {
+    return (
+        <div style={{ display: 'flex', marginTop: 170, width: '100%', height: '75vh' }}>
+            <div style={{ flex: 1 }}>
+                <h1 style={{ color: '#fbbf24', fontSize: 56, textAlign: 'center' }}>Welcome to Ziggurat XYZ</h1>
+                <p style={{ color: 'white', fontSize: 36, textAlign: 'center' }}>We’re a zealous gang of specialists quietly helping creators, whatever their size, make their millions.</p>
+                <button className="explore-button">Explore the Ziggurat</button>
+            </div>
+            <img src={heroimg} style={{ flex: 1, objectFit: 'cover', width: 400, height: 500 }} />
+        </div>
+    )
+}
+
+export default HeroComponent
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../App.css';
+import svg from '../imgs/logo.svg';
+
+function NavBar() {
+    return (
+        <div style={{position:'fixed', width: '100vw', top: 20}}>
+            <a style={{ width: '90%', height: 70 }} className="codepen-button">
+                <div>
+                    <div>
+                        <img src={svg} width='200px' height='200px' />
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'space-around', fontSize: 22 }}>
+                        <div className="underline-color" style={{ textAlign: 'center', display: 'block', padding: 10, marginRight: 10 }}>
+                            About
+                        </div>
+                        <div className="underline-color" style={{ textAlign: 'center', display: 'block', padding: 10, marginRight: 10 }}>
+                            Services
+                        </div>
+                        <div className="underline-color" style={{ textAlign: 'center', display: 'block', padding: 10, marginRight: 10 }}>
+                            Summit
+                        </div>
+                        <div className="underline-color" style={{ textAlign: 'center', display: 'block', padding: 10, marginRight: 10 }}>
+                            Carrers
+                        </div>
+                        <div className="contactus" style={{ textAlign: 'center', display: 'block', padding: 10, border: '2px solid white', marginRight: 10, borderRadius: 5 }}>
+                            Contact Us
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    )
+}
+
+export default NavBar
